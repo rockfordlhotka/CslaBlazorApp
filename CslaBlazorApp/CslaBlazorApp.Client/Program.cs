9410ac064702cdfe8d154a2c29bf0ad3be648ba8
Rockford lhotka <rocky@lhotka.net>
2024-01-02 14:43:33 -0600

    Version that works with latest CSLA code

--- CslaBlazorApp/CslaBlazorApp.Client/Program.cs
+++ CslaBlazorApp/CslaBlazorApp.Client/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddCsla(opt => opt
-    .AddBlazorWebAssembly());
+    .AddBlazorWebAssembly(o => o.SyncContextWithServer = true));
 
 await builder.Build().RunAsync();
